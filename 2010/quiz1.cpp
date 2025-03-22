60d9a5260da3bbcb27032bc4a723b4a2cf86afe8
frank <liuguozhu720@gmail.com>
2025-03-22 16:44:11 +0800

    2010 update

--- 2010/quiz1.cpp
+++ 2010/quiz1.cpp
@@ -1,68 +1,40 @@
-#include <cassert>
 #include <fstream>
-#include <iostream>
 #include <sstream>
-#include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-ifstream inputFile("input.txt");
-
-char toggleCase(char c)
-{
-    return isupper(c) ? tolower(c) : toupper(c);
-}
-
-int main()
-{
+int main() {
+    ifstream inputFile("input.txt");
     stringstream buffer;
     buffer << inputFile.rdbuf();
-    string str = buffer.str();
 
-    string         res;
     vector<string> lines;
-    string         currentLine;
-
-    for (size_t i = 0; i < str.size(); ++i) {
-        char c = str[i];
-
+    string currentLine;
+    string article = buffer.str();
+    for (int i = 0; i < article.length(); i++) {
+        char c = article[i];
         if (isalpha(c) || c == ' ') {
-            currentLine += c;
+            currentLine.push_back(c);
         } else if (c == '!') {
-            if (!currentLine.empty()) {
-                currentLine.pop_back();
-            }
-        } else if (c == '*') {
-            if (!lines.empty()) {
-                lines.pop_back();
-            }
+            currentLine.pop_back();
+        } else if (c == '*' && !lines.empty()) {
+            lines.pop_back();
         } else if (c == '>') {
-            if (!currentLine.empty()) {
-                int j = currentLine.size() - 1;
-                while (j >= 0 && currentLine[j] == ' ')
-                    --j;
-                while (j >= 0 && isalpha(currentLine[j]))
-                    --j;
-                if (j + 1 < currentLine.size()) {
-                    currentLine[j + 1] = toggleCase(currentLine[j + 1]);
-                }
-            }
-        } else if (isdigit(c)) {
-            continue;
-        } else if (c == '\n') {
+            int j = i - 1;
+            while (j >= 0 && currentLine[j] == ' ') j--;
+            while (j >= 0 && currentLine[j] != ' ') j--;
+            currentLine[j + 1] = toupper(currentLine[j + 1]);
+        } else if (isdigit(c)) continue;
+        else if (c == '\n') {
             lines.push_back(currentLine);
             currentLine.clear();
         }
     }
+    if (!currentLine.empty()) lines.push_back(currentLine);
 
-    if (!currentLine.empty()) {
-        lines.push_back(currentLine);
-    }
-
-    for (const auto &line : lines) {
-        cout << line << endl;
-    }
+    for (auto& line : lines) cout << line << endl;
 
     return 0;
 }
